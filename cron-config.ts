f20371dd28604ccf4bc3fcac7f03f0e8e1bcf280
Mike Cann <mike.cann@gmail.com>
2026-03-11 11:48:38 +0800

    Add heartbeat cron example

--- cron-config.ts
+++ cron-config.ts
@@ -1,3 +1,12 @@
 import type { CronJob } from "./cron.ts"
 
-export const cronJobs: CronJob[] = []
+export const cronJobs: CronJob[] = [
+  {
+    name: "heartbeat",
+    intervalMs: 30 * 60 * 1000,
+    runOnStart: true,
+    run: async ({ log }) => {
+      log("INFO", "Heartbeat cron job ran")
+    },
+  },
+]
